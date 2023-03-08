6f40b9e2011e8710b3bf1de0dfe49b099c8bc2bb
SimonLiBFS <110241238+SimonLiBFS@users.noreply.github.com>
2023-03-08 13:11:33 -0500

    Add files via upload

---
+++ SpringSecurityDemo/SpringSecurityAuth/src/main/java/com/beaconfire/springsecurityauth/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.beaconfire.springsecurityauth.config;
+
+import com.beaconfire.springsecurityauth.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
+        return authenticationManagerBuilder.build();
+    }
+
+    //This method is used to configure the security of the application
+    //Since we are attaching jwt to a request header manually, we don't need to worry about csrf
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        return http.build();
+    }
+}
